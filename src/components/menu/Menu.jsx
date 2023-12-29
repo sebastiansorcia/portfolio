328b36c2488016dca245d324d594ebc76783f26a
sebastian <sebastianseberianodev@gmail.com>
2023-12-29 13:09:43 -0600

    se cambio el nombre del enlace

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
         <div className="menu__header">
             <ul className="list__items">
                 <li className="list__item"><a href="#home" className="link__item item" aria-label="seccion de inicio">inicio</a></li>
-                <li className="list__item"><a href="#aboutme" className="link__item item" aria-label="seccion sobre mi">sobre mi</a></li>
+                <li className="list__item"><a href="#ability" className="link__item item" aria-label="seccion sobre mi">habilidades</a></li>
                 <li className="list__item"><a href="#proyects" className="link__item item"aria-label="seccion de proyectos">proyectos</a></li>
                 <li className="list__item"><ButtonCt content="Contáctame"/></li>
             </ul>
